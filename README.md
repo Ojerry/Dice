1b2579f77989adc081dc35c288a413467b854206
Ojerry <onotu128@gmail.com>
2022-04-19 03:15:15 -0700

    Class Added

---
+++ README.md
@@ -0,0 +1,1 @@
+A very Simple Dice Game
--- app/src/main/java/app/ojinc/dice/Inheritance.kt
+++ app/src/main/java/app/ojinc/dice/Inheritance.kt
@@ -1,13 +1,19 @@
     package app.ojinc.dice
 
     class Inheritance {
+        val jeep = Jeep(2019)
     }
 
-    abstract class car(){
-          abstract val color: String
-          abstract val name: String
+ abstract class car(var releaseYear: Int){
+     abstract val  color: String
+         abstract val name: String
             fun cost(DollarPrice: Int): Int {
-                val NairatoDollar = 567
-        return DollarPrice * NairatoDollar
+                val NairaToDollar = 567
+                return DollarPrice * NairaToDollar
          }
+    }
+
+    class Jeep(releaseYear: Int): car(releaseYear){
+        override val color = "Black"
+        override val name = "PathFinder"
     }
